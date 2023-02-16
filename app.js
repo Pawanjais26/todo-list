d681bed3c88f1ac4e0d5ce9407149a8ee096fe33
Pawan <Pawanjais2000@gmail.com>
2023-02-16 15:10:51 +0530

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ mongoose.set("strictQuery", true);
 const _ = require("lodash");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
@@ -139,11 +140,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-};
-
-app.listen(port, function () {
-  console.log("Server is running on port 3000.");
+app.listen(PORT, () => {
+  console.log("listening for requests");
 });
